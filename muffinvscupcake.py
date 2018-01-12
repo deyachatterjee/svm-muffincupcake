c5923d4046bc5591e6d636ecc43acf05d090513c
Deya Chatterjee <28012711+deyachatterjee@users.noreply.github.com>
2018-01-12 09:26:21 +0530

    Update muffinvscupcake.py

--- muffinvscupcake.py
+++ muffinvscupcake.py
@@ -1,3 +1,65 @@
-# svm-muffincupcake
-This is the implementation of Alice Zhao's youtube video which attempts to classify muffins vs. cupcakes using Support Vector Machines.
-Library used: scikit-learn
+import pandas as pd
+import numpy as np
+from sklearn import svm
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set(font_scale=1.2)
+
+ 
+#load data
+recipes=pd.read_csv('recipes_muffins_cupcakes.csv')
+
+#plot data as two ingredients on two axes
+sns.lmplot('Flour', 'Sugar', data=recipes, hue='Type',
+           palette='Set1', fit_reg=False, scatter_kws={"s": 70});
+
+#model inputs		   
+ingredients = recipes[['Flour','Sugar']].as_matrix()
+type_label = np.where(recipes['Type']=='Muffin', 0, 1)
+recipe_features = recipes.columns.values[1:].tolist()		
+
+#now we are fitting the svm model using sklearn's svc 
+model = svm.SVC(kernel='linear')
+model.fit(ingredients, type_label)
+
+#visualise the hyperplanes
+# Get the separating hyperplane
+w = model.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(30, 60)
+yy = a * xx - (model.intercept_[0]) / w[1]
+
+# Plot the parallels to the separating hyperplane that pass through the support vectors
+b = model.support_vectors_[0]
+yy_down = a * xx + (b[1] - a * b[0])
+b = model.support_vectors_[-1]
+yy_up = a * xx + (b[1] - a * b[0])
+
+#now we will plot the hyperplane
+sns.lmplot('Flour', 'Sugar', data=recipes, hue='Type', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color='black');
+
+
+#we know that the best hyperplane is the one with max margin
+sns.lmplot('Flour', 'Sugar', data=recipes, hue='Type', palette='Set1', fit_reg=False, scatter_kws={"s": 70})
+plt.plot(xx, yy, linewidth=2, color='black')
+plt.plot(xx, yy_down, 'k--')
+plt.plot(xx, yy_up, 'k--')
+plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
+            s=80, facecolors='none');
+			
+			
+#predict function
+def muffin_or_cupcake(flour, sugar):
+    if(model.predict([[flour, sugar]]))==0:
+        print('muffin')
+    else:
+        print('cupcake')
+		
+
+# Predict if 50 parts flour and 20 parts sugar, for example
+muffin_or_cupcake(50, 20) 
+#outputs 'muffin'
+
+"""
+Pickling is just serialization: putting data into a form that can be stored in a file and retrieved later. Pickling is a way to convert a python object (list, dict, etc.) into a character stream. The idea is that this character stream contains all the information necessary to reconstruct the object in another python script.
+"""
