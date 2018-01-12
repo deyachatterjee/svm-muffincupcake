fedc7e2218853d1af04077e6f5977eed8bd6fdc0
Deya Chatterjee <28012711+deyachatterjee@users.noreply.github.com>
2018-01-12 09:05:52 +0530

    Create muffinvscupcake.py

---
+++ muffinvscupcake.py
@@ -0,0 +1,3 @@
+# svm-muffincupcake
+This is the implementation of Alice Zhao's youtube video which attempts to classify muffins vs. cupcakes using Support Vector Machines.
+Library used: scikit-learn
